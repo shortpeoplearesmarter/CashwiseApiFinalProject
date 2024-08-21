b2001d539bf365de6b9277f684dabc0823c1a998
Aigera <ajgerasydykova@gmail.com>
2024-08-21 17:39:47 -0500

    added Geras Branch

--- src/test/java/entities/CustomResponse.java
+++ src/test/java/entities/CustomResponse.java
@@ -1,4 +1,22 @@
 package entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CustomResponse {
+
+    private String category_title;
+//    private String email;
+
+    //get a category id;
+    private int category_id;
+    List<CustomResponse> responses;
+
+    private int seller_id;
+    private String email;
+
 }
